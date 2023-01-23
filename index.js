4c6b726b99b6f59f118476e244aff81575ed9697
Naomiii <66597376+Na0xmi@users.noreply.github.com>
2023-01-23 13:17:07 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+//let homeScoreBtnOne = document.getElementById("home-score-btn-1")
+let homeScoreEl =document.getElementById("home-score")
+let homeScore = 0
+
+function homeAddOne(){
+    homeScore += 1
+    homeScoreEl.textContent = homeScore
+}
+
+function homeAddTwo(){
+    homeScore += 2
+    homeScoreEl.textContent = homeScore
+}
+
+function homeAddThree(){
+    homeScore += 3
+    homeScoreEl.textContent = homeScore
+}
+
+let guestScoreEl = document.getElementById("guest-score")
+let guestScore = 0 
+
+function guestAddOne() {
+    guestScore += 1 
+    guestScoreEl.textContent = guestScore
+}
+
+function guestAddTwo() {
+    guestScore +=2
+    guestScoreEl.textContent = guestScore
+}
+
+function guestAddThree() {
+    guestScore +=3
+    guestScoreEl.textContent = guestScore
+}
